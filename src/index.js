d7d4b7f6b373deae0d679aa6f30b0588ae1211ea
unknown <nadezhda.1.kotova@gmail.com>
2019-02-24 23:13:16 +0300

    Implemented function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBracket = [];
+  let closeBracket = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      openBracket[i] = bracketsConfig[i][0];
+      closeBracket[i] = bracketsConfig[i][1];
+  };
+  let arrayOfData = [];
+  for (let i = 0; i < str.length; i++) {
+      let bracket = str[i];
+      let index = openBracket.indexOf(bracket);
+      if (index >= 0) {
+          if (index == arrayOfData[arrayOfData.length - 1] && closeBracket[index] == openBracket[index]) {
+              arrayOfData.pop();
+          } else {
+              arrayOfData.push(index);
+          }
+      } else if (closeBracket.indexOf(bracket) == arrayOfData[arrayOfData.length - 1]) {
+          arrayOfData.pop();
+      } else {
+          arrayOfData.push('!=');
+      }
+  }
+  return arrayOfData.length ? false : true;
 }
